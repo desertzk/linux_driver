746539f76e3fcdbe489e085de6e92c6edef49cab
ka zhang <kazhang2@amd.com>
2023-11-04 08:29:05 -0400

    print memblock

--- my_driver/kmalloc_demo/kmalloc_demo.c
+++ my_driver/kmalloc_demo/kmalloc_demo.c
@@ -1,6 +1,10 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+
 
 struct student{
 	int id;
@@ -28,12 +32,17 @@ static int __init hello_init(void){
 	printk("phys_base %lx\n",phys_base);
 	printk("page_offset_base %lx\n",page_offset_base);
 	printk("vmalloc_base %lx\n",vmalloc_base);
+	printk("VMALLOC_START %lx\n",VMALLOC_START);
+	printk("VMALLOC_END %lx\n",VMALLOC_END);
 	printk("vmemmap_base %lx\n",vmemmap_base);
 	printk("PAGE_OFFSET %lx  __START_KERNEL_map %lx  \n",PAGE_OFFSET,__START_KERNEL_map);
 /*	    printk(KERN_INFO "Kernel Text (Code): 0x%lx - 0x%lx\n", (unsigned long)_stext, (unsigned long)_etext);
     printk(KERN_INFO "Kernel Data: 0x%lx - 0x%lx\n", (unsigned long)_sdata, (unsigned long)_edata);
     printk(KERN_INFO "Init Task: 0x%lx\n", (unsigned long)&init_task);
     printk(KERN_INFO "Kernel Stack (current): 0x%lx\n", (unsigned long)current);*/
+
+
+    printk(KERN_INFO "Kernel Stack (current): 0x%lx\n", (unsigned long)current);
 	return 0;
 
 }
