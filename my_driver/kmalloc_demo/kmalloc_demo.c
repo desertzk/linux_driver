aafdcd165f3ddded0a9169fa5b118268ec4e6a03
desertzk <309410281@qq.com>
2023-11-14 09:16:02 +0800

    fix compile error on linux5.15

--- my_driver/kmalloc_demo/kmalloc_demo.c
+++ my_driver/kmalloc_demo/kmalloc_demo.c
@@ -4,7 +4,8 @@
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
-
+#include<linux/vmalloc.h>
+#include<linux/io.h>
 
 struct student{
 	int id;
@@ -21,8 +22,6 @@ void stu_ctor(void *p)
 //init objects here
 //
 
-
-
 void *q;
 void *vp;
 
@@ -36,13 +35,19 @@ static int __init hello_init(void){
 	printk("sizeof student %ld\n",sizeof(*p));
 	printk("p phy %llx\n",virt_to_phys(p));
 
+
+	struct mm_struct init_mm = *(current->mm);
 	printk("phys_base %lx\n",phys_base);
 	printk("page_offset_base %lx\n",page_offset_base);
 	printk("vmalloc_base %lx\n",vmalloc_base);
 	printk("VMALLOC_START %lx\n",VMALLOC_START);
 	printk("VMALLOC_END %lx\n",VMALLOC_END);
 	printk("vmemmap_base %lx\n",vmemmap_base);
+	printk("current pid %lx\n",current->pid);
+	printk("current_mm kernel code start  %lx end %lx \n",init_mm.start_code,init_mm.end_code);
+	printk("current_mm kernel data start %lx end %lx \n",init_mm.start_data,init_mm.end_data);
 	printk("PAGE_OFFSET %lx  __START_KERNEL_map %lx  \n",PAGE_OFFSET,__START_KERNEL_map);
+	
 /*	    printk(KERN_INFO "Kernel Text (Code): 0x%lx - 0x%lx\n", (unsigned long)_stext, (unsigned long)_etext);
     printk(KERN_INFO "Kernel Data: 0x%lx - 0x%lx\n", (unsigned long)_sdata, (unsigned long)_edata);
     printk(KERN_INFO "Init Task: 0x%lx\n", (unsigned long)&init_task);
